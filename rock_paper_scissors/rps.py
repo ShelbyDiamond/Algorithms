06c455257c8fca7bcc40a58405c90d1c488694c6
Shelby Diamond <Shelbydiamond518@gmail.com>
2020-01-23 21:20:13 -0700

    updated rock_paper_scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,25 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+def rock_paper_scissors(n: int) -> list:
+   choices = ["rock", "paper", "scissors"]
+   combinations = []
 
+   def help(b: int, result: list):
+     if b == 0:
+        combinations.append(result)
+        return
+      for i in choices:
+        help(b - 1, result + [i])
+    help(n, [])
+   return combinations
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+print(rock_paper_scissors(1))
+print(rock_paper_scissors(2))
+
+# if __name__ == "__main__":
+#  if len(sys.argv) > 1:
+#    num_plays = int(sys.argv[1])
+#    print(rock_paper_scissors(num_plays))
+#  else:
+#    print('Usage: rps.py [num_plays]')
